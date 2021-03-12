6391058941ebcb9f71619aa1077a992220b82a35
Fateblade <Fateblade@users.noreply.github.com>
2021-03-12 23:34:21 +0100

    add component activator for translation component

---
+++ Logic.Foundation.Translation/_TranslationComponentActivator.cs
@@ -0,0 +1,41 @@
+﻿using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.Bootstrapping;
+using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.Configuration;
+using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.DependencyInjection;
+using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.DependencyInjection.DataClasses;
+using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.EventBrokerage;
+using Fateblade.Components.Logic.Foundation.Translation.Contract;
+
+namespace Fateblade.Components.Logic.Foundation.Translation
+{
+    public class TranslationComponentActivator : IComponentActivator
+    {
+        public void Activating()
+        {
+        }
+
+        public void Activated()
+        {
+        }
+
+        public void Deactivating()
+        {
+        }
+
+        public void Deactivated()
+        {
+        }
+
+        public void RegisterMappings(ICoCoKernel kernel)
+        {
+            kernel.Register<ITranslationStringProvider, TranslationStringProvider>(RegisterScope.Unique);
+        }
+
+        public void AddMessageSubscriptions(IEventBroker broker)
+        {
+        }
+
+        public void Configure(IConfigurator config)
+        {
+        }
+    }
+}
