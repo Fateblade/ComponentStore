1946802a14442ddef32449e3d71de80b08842d16
Fateblade <Fateblade@users.noreply.github.com>
2024-05-08 09:19:10 +0200

    add component activator for delayed action executor

--- Logic/Foundation/Logic.Foundation.DelayedDelegateExecutor/_DelayedActionExecutionComponentActivator.cs
+++ Logic/Foundation/Logic.Foundation.DelayedDelegateExecutor/_DelayedActionExecutionComponentActivator.cs
@@ -1,6 +1,41 @@
-﻿namespace Fateblade.Components.Logic.Foundation.DelayedActionExecution
+﻿using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.Bootstrapping;
+using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.Configuration;
+using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.DependencyInjection;
+using DavidTielke.PersonManagementApp.CrossCutting.CoCo.Core.Contract.EventBrokerage;
+using Fateblade.Components.Logic.Foundation.DelayedActionExecution.Contract;
+
+namespace Fateblade.Components.Logic.Foundation.DelayedActionExecution
 {
-    class _DelayedActionExecutionComponentActivator
+    public class DelayedActionExecutionComponentActivator : IComponentActivator
     {
+        public void Activating()
+        {
+        }
+
+        public void Activated()
+        {
+        }
+
+        public void Deactivating()
+        {
+        }
+
+        public void Deactivated()
+        {
+        }
+
+        public void RegisterMappings(ICoCoKernel kernel)
+        {
+            kernel.RegisterConfiguration<DelayedActionExecutionConfiguration>();
+            kernel.Register<IDelayedActionExecutor, DelayedActionExecutor>();
+        }
+
+        public void AddMessageSubscriptions(IEventBroker broker)
+        {
+        }
+
+        public void Configure(IConfigurator config)
+        {
+        }
     }
 }
