90c6ae8eee0562ab46b8371abd407e1f33077c91
Fateblade <Fateblade@users.noreply.github.com>
2024-05-22 21:25:00 +0200

    remove non components

--- UI/Console/UI.Console.JsonDataModifier/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using UI.Console.JsonDataModifier;
-
-StoreFileModifier.ModifyBudgetValues(1000000);
-AirframesDataFileModifier.ModifyDriveValues(1.5f, 0.5f, 0.5f, 0.8f);
